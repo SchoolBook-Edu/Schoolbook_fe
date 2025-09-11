2df12302df6a7b84197913c4931a75629f520f81
A I Walter <idams91k@gmail.com>
2025-09-11 18:12:22 +0100

    Updated axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://schoolbook-api.onrender.com'
+    // baseURL: 'https://schoolbook-api.onrender.com'
+    baseURL: 'http://localhost:5001',
 })
